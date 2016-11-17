77dd0151d5b1b38e02ec4fa5dacd7d69cf251ca1
ejank <jankowska.ej@gmail.com>
2016-11-17 12:22:11 +0100

    initial commit

---
+++ page/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.homepage, name='homepage'),
+    url(r'^events/$', views.events, name="events"),
+    url(r'^program/$', views.program, name="program"),
+    url(r'^signup/$', views.signup, name="signup"),
+    url(r'^organizers/$', views.organizers, name="organizers"),
+    
+]
---
+++ page/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Event
+from django.utils import timezone
+
+def homepage(request):
+    latest = Event.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0]
+    last_but_one = Event.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[1]
+    return render(request, 'page/homepage.html', {'latest':latest, 'last_but_one':last_but_one})
+
+def events(request):
+    events = Event.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'page/events.html', {'events':events})
+
+def program(request):
+    return render(request, 'page/program.html', {})
+
+def signup(request):
+    return render(request, 'page/signup.html', {})
+
+def organizers(request):
+    return render(request, 'page/organizers.html', {})
